08e49579a2909405209de397b88710dc1afeaae3
DaMao <blitecorful@gmail.com>
2024-07-10 16:52:11 +0800

    Add project files.

---
+++ XtrayUserInterface/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+
+namespace XtrayUserInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            this.Portname.ItemsSource = ports;
+            this.Portname.SelectedIndex = 1;
+
+            string[] baudrate = new string[] { "57600", "115200"};
+            this.BaudRate.ItemsSource = baudrate;
+            this.BaudRate.SelectedIndex = 1;
+
+        }
+    }
+}
